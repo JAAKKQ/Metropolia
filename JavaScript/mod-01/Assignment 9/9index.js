84b56191a14c1817112db64fbb322ebfc2072c30
Rene <rene@karkkainen.net>
2025-04-03 09:52:50 +0300

    9

--- JavaScript/mod-01/Assignment 9/9index.js
+++ JavaScript/mod-01/Assignment 9/9index.js
@@ -1,2 +1,20 @@
 'use strict';
 const number = parseInt(prompt("Give me a number."))
+
+if (number <= 1) {
+    document.querySelector('#target').innerHTML = `${number} is not a prime number :(`
+} else {
+    let isPrime = true;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        document.querySelector('#target').innerHTML = `${number} is a prime number!`
+    } else {
+        document.querySelector('#target').innerHTML = `${number} is not a prime number :(`
+    }
+}
+
